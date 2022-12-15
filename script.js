30b604fa3058602c2df3ebc1de0a262a081180a4
Paul Bakeman <pandmbake@comcast.net>
2022-12-15 12:14:30 -0500

    delete color change button and reset button

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function showTime() {
     am_pm = "AM";
  
     if (hour >= 12) {
-        hour -= 12;
+        //hour -= 12;
         am_pm = "PM";
     }
     if (hour == 0) {
@@ -26,7 +26,7 @@ function showTime() {
             .innerHTML = currentTime;
 
 
-    /*if (am_pm === "AM") {
+    if (am_pm === "AM") {
         document.getElementById("greeting").innerHTML = "Good Morning!"
     }
 
@@ -38,60 +38,18 @@ function showTime() {
         document.getElementById("greeting").innerHTML = "Good Afternoon!"
     }
 
-    if (am_pm === "PM" && hour >= 6) {
+    if (am_pm === "PM" && hour >= 6 && hour < 12) {
         document.getElementById("greeting").innerHTML = "Good Evening!"
-    }*/
+    }
 
         }
 
 
-        function colorChange() {
-            document.getElementById('h2').style.color = randomColors();
-            document.getElementById('main').style.backgroundColor = randomColors();
-            document.getElementById('clock').style.color = randomColors();
-            document.getElementById('greeting').style.color = randomColors();
-        function randomColors() {
-            return '#' + Math.floor(Math.random() * 16777215).toString(16);
-          }
         
-          }
+        
+          
 
-          function resetColors() {
-            document.getElementById('h2').style.color = 'orange';
-            document.getElementById('h2').style.fontSize = '3em';
-            document.getElementById('h2').style.marginTop = '2em';
-            document.getElementById('h2').innerHTML = '8-Bit Clock';
-            document.getElementById('main').style.backgroundImage = 'url("images/R (1).jpeg")';
-            document.getElementById('clock').style.color = 'black';
-            document.getElementById('clock').style.width= '700px';
-            document.getElementById('clock').style.padding = '10px';
-            document.getElementById('clock').style.border = 'solid 4px orange';
-            document.getElementById('clock').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('clock').style.fontSize = "40px";
-            document.getElementById('clock').style.marginTop = "0.01em";
-            document.getElementById('greeting').style.color = 'orange';
-            document.getElementById('greeting').style.fontSize = '1.5em';
-            document.getElementById('greeting').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('greeting').style.marginTop = "2em";
-            document.getElementById('greeting').style.marginBottom = "1em";
-            document.getElementById('h2').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('creepy').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('typewriter').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('dimensional').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('creepy').style.fontSize = ".7em";
-            document.getElementById('typewriter').style.fontSize = ".7em";
-            document.getElementById('dimensional').style.fontSize = ".7em";
-            document.getElementById('creepy').style.width = "30em";
-            document.getElementById('typewriter').style.width = "30em";
-            document.getElementById('dimensional').style.width = "30em";
-            document.getElementById('changeColor').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('resetColors').style.fontFamily = "'Press Start 2P', cursive";
-            document.getElementById('changeColor').style.fontSize = ".7em";
-            document.getElementById('resetColors').style.fontSize = ".7em";
-            
-            
-    
-          }
+  
 
           function creepy() {
             document.getElementById('h2').style.fontFamily = "'Creepster', cursive";
