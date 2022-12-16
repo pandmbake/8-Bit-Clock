259994ddbf056251327ff531a61d9aa359424fe6
Paul Bakeman <pandmbake@comcast.net>
2022-12-16 12:43:28 -0500

    fix greeting code

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-setInterval(showTime, 1000);
-function showTime() {
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
-  am_pm = "AM";
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
 
-  if (hour >= 12) {
-    hour -= 12;
-    am_pm = "PM";
-  }
-  if (hour == 0) {
-    hr = 12;
-    am_pm = "AM";
-  }
-
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
-
-  let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
-
-  document.getElementById("clock").innerHTML = currentTime;
-
-  if (am_pm === "AM") {
+  if (h < 12) {
     document.getElementById("greeting").innerHTML = "Good Morning!";
-  }
-
-  if (am_pm === "PM" && hour === 12) {
+  } else if (h < 18) {
     document.getElementById("greeting").innerHTML = "Good Afternoon!";
-  }
-
-  if (am_pm === "PM" && hour >= 1) {
-    document.getElementById("greeting").innerHTML = "Good Afternoon!";
-  }
-
-  if (am_pm === "PM" && hour >= 6 && hour < 12) {
+  } else {
     document.getElementById("greeting").innerHTML = "Good Evening!";
   }
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
 }
 
 function resetColors() {
@@ -198,8 +182,6 @@ function dimensional() {
   document.getElementById("typewriter").style.display = "flex";
   document.getElementById("creepy").style.display = "flex";
 }
-
-
 
 //font-family: 'Creepster', cursive;
 //font-family: 'Special Elite', cursive;
